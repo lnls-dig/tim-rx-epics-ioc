a1051fd715e3d53a06f4cce8ce0b88ac9c1c4fa3
Lucas Russo <lerwys@gmail.com>
2018-03-09 09:58:04 -0300

    TimRxApp: fix getParamDouble not returning correct values
    This is the same issue as lnls-dig/bpm-epics-ioc#26.

--- TimRxApp/src/drvTimRx.cpp
+++ TimRxApp/src/drvTimRx.cpp
@@ -708,6 +708,7 @@ asynStatus drvTimRx::getParamDouble(int functionId, epicsFloat64 *param, int add
     }
 
     status = executeHwReadFunction(functionId, addr, functionArgs);
+    *param = functionArgs.argFloat64;
 
 get_param_err:
     return status;
