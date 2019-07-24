fe075f74aeaa2eaafc1576a33c3a5cd8a0f2bdd4
Lucas Russo <lerwys@gmail.com>
2019-07-24 14:29:29 -0300

    iocBoot/iocTimRx: disable mlockall() as it's causing problems in CentOS7
    See https://epics.anl.gov/base/R3-15/6-docs/RELEASE_NOTES.html,
    "Moved mlockall() into its own epicsThread routine".

--- iocBoot/iocTimRx/stTimRx.cmd
+++ iocBoot/iocTimRx/stTimRx.cmd
@@ -44,6 +44,13 @@ asynSetTraceIOMask("$(TIM_RX_NAME)",0,0x2)
 #asynSetTraceMask("", 0, 17)
 #asynSetTraceMask("$(TIM_RX_NAME)",0,0xff)
 
+# Disable locking virtual memory as it can increase memory usage
+# unnecessarily in systems that run the IOC with SCHED_FIFO and
+# supports running threads with different priorities. See:
+# https://epics.anl.gov/base/R3-15/6-docs/RELEASE_NOTES.html and
+# https://github.com/epics-base/epics-base/commit/e721be4ff528bc1fff35b9e0cffd2a194f3e3675
+var dbThreadRealtimeLock 0
+
 iocInit()
 
 < initTimRxCommands
